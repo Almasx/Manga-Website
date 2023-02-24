24a4cb70c422d4acf7ec605e4554b04240de94e3
Almasx <saparovalmas0@gmail.com>
2023-02-25 01:44:45 +0500

    edit page

--- src/pages/comics/[comicsId]/edit.tsx
+++ src/pages/comics/[comicsId]/edit.tsx
@@ -1,5 +1,11 @@
+import ModifyComics from "components/templates/ModifyComics";
+
 const EditComics = () => {
-  return <div>EditComics</div>;
+  return (
+    <div className="mx-40 mt-3 w-[80vw]">
+      <ModifyComics />
+    </div>
+  );
 };
 
 export default EditComics;
--- src/pages/layout/main.tsx
+++ src/pages/layout/main.tsx
@@ -1,11 +1,8 @@
 import GridLayout from "core/ui/templates/GridLayout";
 import Navigation from "core/ui/templates/Navigation";
+import type { ReactNode } from "react";
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative min-h-screen overflow-hidden bg-black text-white">
       <Navigation.Wrapper auth={true}>
