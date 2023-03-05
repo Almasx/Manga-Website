bc023623d67ca0c4af65163d26a459d30e999df0
Almasx <saparovalmas0@gmail.com>
2023-03-06 01:50:14 +0500

    deploy fix

--- src/lib/aws/aws-config.ts
+++ src/lib/aws/aws-config.ts
@@ -3,8 +3,8 @@ import * as aws from "aws-sdk";
 import { env } from "../../env/server.mjs";
 
 aws.config.update({
-  accessKeyId: env.AWS_ACCESS_KEY,
-  secretAccessKey: env.AWS_SECRET_KEY,
+  accessKeyId: env.AWS_ACCESS_KEY_ID_ID,
+  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   region: env.AWS_REGION,
   signatureVersion: "v4",
 });
