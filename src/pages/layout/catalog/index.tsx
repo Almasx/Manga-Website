3d787adfc2c9b7b3fdf68661bd7a7e4aa332809e
Almasx <saparovalmas0@gmail.com>
2023-03-14 15:51:31 +0500

    catalog adaptive

--- src/pages/layout/catalog/index.tsx
+++ src/pages/layout/catalog/index.tsx
@@ -15,25 +15,21 @@ export default function CatalogLayout({ children }: { children: ReactNode }) {
         <Navigation.Links />
       </Navigation.Wrapper>
 
-      <main
-        className="relative mx-auto grid w-full grid-cols-4 gap-5 px-4
-                  md:grid-cols-8 lg:grid-cols-12 lg:px-0"
-      >
+      <main className="relative mx-auto w-full flex-row">
         {children}
         <FilterSideBar show={showSideBar} setShow={setShowSideBar} />
-
-        {isDesktop && (
-          <div className="fixed bottom-[15px] right-[15px] z-10">
-            <Button
-              className="h-9 w-9 rounded-2xl text-light md:hidden"
-              content="icon"
-              onClick={() => setShowSideBar(true)}
-            >
-              <FilterSearch size={18} />
-            </Button>
-          </div>
-        )}
       </main>
+      {!isDesktop && (
+        <div className="fixed bottom-[15px] right-[15px]">
+          <Button
+            className="z-10 h-9 w-9 rounded-2xl text-light"
+            content="icon"
+            onClick={() => setShowSideBar(true)}
+          >
+            <FilterSearch size={18} />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
